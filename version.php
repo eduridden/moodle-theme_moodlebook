528c43f5cbece301a84ad045e1b48ad7e73bf85d
Julian Ridden <julian@moodleman.net>
2012-01-10 16:06:09 +1100

    Adding a version.php to meet moodle coding guidlelines

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2012010200; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2010112400; // Requires this Moodle version
+$plugin->component = 'theme_moodlebook';
+$plugin->maturity  = MATURITY_STABLE;
