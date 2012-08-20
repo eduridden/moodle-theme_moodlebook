b6f239017311b3f3d119841b4f5db1d485bcb526
Julian Ridden <julian@moodleman.net>
2012-08-20 21:43:06 +1000

    various updates for 2.3 compatibility brought in from decaf theme

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Settings for the moodlebook theme
+ * Settings for the Moodlebook theme
  */
 
 defined('MOODLE_INTERNAL') || die;
@@ -12,7 +12,7 @@ if ($ADMIN->fulltree) {
     $name = 'theme_moodlebook/backgroundcolor';
     $title = get_string('backgroundcolor','theme_moodlebook');
     $description = get_string('backgroundcolordesc', 'theme_moodlebook');
-    $default = '#fff';
+    $default = '#EEE';
     $previewconfig = array('selector'=>'html', 'style'=>'backgroundColor');
     $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
     $settings->add($setting);
@@ -48,4 +48,33 @@ if ($ADMIN->fulltree) {
     $choices = array(1=>'Yes', 0=>'No');
     $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
     $settings->add($setting);
+    
+    // Show user profile picture
+    $name = 'theme_moodlebook/showuserpicture';
+    $title = get_string('showuserpicture','theme_moodlebook');
+    $description = get_string('showuserpicturedesc', 'theme_moodlebook');
+    $default = 0;
+    $choices = array(1=>'Yes', 0=>'No');
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $settings->add($setting);
+
+    // Add custom menu to Awesomebar
+    $name = 'theme_moodlebook/custommenuinawesomebar';
+    $title = get_string('custommenuinawesomebar','theme_moodlebook');
+    $description = get_string('custommenuinawesomebardesc', 'theme_moodlebook');
+    $default = 0;
+    $choices = array(1=>'Yes', 0=>'No');
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $settings->add($setting);
+
+    // Place custom menu after Awesomebar
+    $name = 'theme_moodlebook/custommenuafterawesomebar';
+    $title = get_string('custommenuafterawesomebar','theme_moodlebook');
+    $description = get_string('custommenuafterawesomebardesc', 'theme_moodlebook');
+    $default = 0;
+    $choices = array(0=>'No', 1=>'Yes');
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $settings->add($setting);
+   
+
 }
