c993f0384a1d18988c0d74d6c046e62ed64f6a10
Julian Ridden <julian@moodleman.net>
2013-01-10 09:41:01 +1100

    Updated version Information

--- version.php
+++ version.php
@@ -1,7 +1,41 @@
 <?php
-defined('MOODLE_INTERNAL') || die;
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version   = 2012010200; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2010112400; // Requires this Moodle version
+/**
+ * Version details
+ *
+ * @package    format
+ * @subpackage grid
+ * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Plugin version (update when tables change)
+$plugin->version  = 2012010200;
+
+// Required Moodle version
+$plugin->requires = 2012120300.00; // 2.4 (Build: 20121203)
+
+// Full name of the plugin (used for diagnostics)
 $plugin->component = 'theme_moodlebook';
+
+// Software maturity level
 $plugin->maturity  = MATURITY_STABLE;
+
+// User-friendly version number
+$plugin->release = '2.4.0.1';
