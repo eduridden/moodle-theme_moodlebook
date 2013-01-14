4d153c28b558f461f8b5badfbaa3d7400ca218fc
Julian Ridden <julian@moodleman.net>
2013-01-15 09:44:19 +1100

    Updated renderer with info to hide settings block
    This code was written by Zei originally for the decaf theme.

--- renderers.php
+++ renderers.php
@@ -275,6 +275,34 @@ class theme_moodlebook_core_renderer extends core_renderer {
         return $content;
     }
 
+	/**
+     * blocks_for_region() overrides core_renderer::blocks_for_region()
+     *  in moodlelib.php. Returns a string
+     * values ready for use.
+     *
+     * @return string
+     */
+    public function blocks_for_region($region) {
+        if (!$this->page->blocks->is_known_region($region)) {
+            return '';
+        }
+        $blockcontents = $this->page->blocks->get_content_for_region($region, $this);
+        $output = '';
+        foreach ($blockcontents as $bc) {
+            if ($bc instanceof block_contents) {
+                if (!($this->page->theme->settings->hidesettingsblock && substr($bc->attributes['class'], 0, 15) == 'block_settings ')
+                        && !($this->page->theme->settings->hidenavigationblock && substr($bc->attributes['class'], 0, 17) == 'block_navigation ')) {
+                    $output .= $this->block($bc, $region);
+                }
+            } else if ($bc instanceof block_move_target) {
+                $output .= $this->block_move_target($bc);
+            } else {
+                throw new coding_exception('Unexpected type of thing (' . get_class($bc) . ') found in list of block contents.');
+            }
+        }
+        return $output;
+    }
+
     // Copied from core_renderer with one minor change - changed $this->output->render() call to $this->render()
     protected function render_navigation_node(navigation_node $item) {
         $content = $item->get_content();
